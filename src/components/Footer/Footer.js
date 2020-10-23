1f1f0b29a5e8ea5cf20688669dde45fc97af79a8
Luís Eduardo <luiseduardo@Luiss-MacBook-Pro.local>
2020-10-23 14:54:57 -0300

    feat: add link in cancel button

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,12 +4,15 @@ import { ContainerContent, ContainerFooter } from "./style";
 import LogoMlabs from "../../assets/icons/logo.svg";
 import MenuIcon from "../../assets/icons/bars.svg";
 import { Button } from "../Button";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <ContainerFooter>
       <ContainerContent>
-        <Button color={"blue"} transparent label={"Cancelar"}></Button>
+        <Link to="/">
+          <Button color={"blue"} transparent label={"Cancelar"}></Button>
+        </Link>
         <Button color={"blue"} outline label={"Rascunho"}></Button>
         <Button color={"blue"} label={"Agendar"}></Button>
       </ContainerContent>
